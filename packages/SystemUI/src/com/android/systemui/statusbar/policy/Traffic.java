1b8792af078e0b295544f13d506a0781b77404b7
cofface <cofface@gmail.com>
2013-09-09 19:25:11 +0800

    StatusBar: Traffic Support (4)
    StatusBar: Traffic Support (4)

---
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Traffic.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (C) 2006 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar.policy;
+
+
+
+import java.text.DecimalFormat;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.net.ConnectivityManager;
+import android.net.TrafficStats;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Traffic extends TextView {
+  private boolean mAttached;
+  boolean showTraffic;
+  Handler mHandler;
+  Handler mTrafficHandler;
+  float speed;
+  float totalRxBytes;
+
+  class SettingsObserver extends ContentObserver {
+    SettingsObserver(Handler handler) {
+      super(handler);
+    }
+
+    void observe() {
+      ContentResolver resolver = mContext.getContentResolver();
+      resolver.registerContentObserver(Settings.System
+          .getUriFor(Settings.System.STATUS_BAR_SHOW_TRAFFIC), false, this);
+    }
+
+    @Override
+    public void onChange(boolean selfChange) {
+      updateSettings();
+    }
+  }
+
+  public Traffic(Context context) {
+    this(context, null);
+  }
+
+  public Traffic(Context context, AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public Traffic(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    mHandler = new Handler();
+    SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+    settingsObserver.observe();
+    updateSettings();
+  }
+
+  @Override
+  protected void onAttachedToWindow() {
+    super.onAttachedToWindow();
+
+    if (!mAttached) {
+      mAttached = true;
+      IntentFilter filter = new IntentFilter();
+      filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+      getContext().registerReceiver(mIntentReceiver, filter, null,
+          getHandler());
+    }
+    updateSettings();
+  }
+
+  @Override
+  protected void onDetachedFromWindow() {
+    super.onDetachedFromWindow();
+    if (mAttached) {
+      getContext().unregisterReceiver(mIntentReceiver);
+      mAttached = false;
+    }
+  }
+
+  private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      String action = intent.getAction();
+      if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+        updateSettings();
+      }
+    }
+  };
+
+  public void updateTraffic() {
+    mTrafficHandler = new Handler() {
+      @Override
+      public void handleMessage(Message msg) {
+          speed = (getTotalReceivedBytes() - totalRxBytes) / 1024;
+          totalRxBytes = getTotalReceivedBytes();
+          if (speed >= 0) {
+            DecimalFormat DecimalFormatfnum = new DecimalFormat("##0");
+            if (speed / 1024 >= 1) {
+                setText(DecimalFormatfnum.format(speed / 1024) + "M/s");
+            } else if (speed <= 1) {
+                setText(DecimalFormatfnum.format(speed * 1024) + "B/s");
+            } else {
+                setText(DecimalFormatfnum.format(speed) + "K/s");
+            }
+          }
+          update();
+          super.handleMessage(msg);
+      }
+    };
+    totalRxBytes = getTotalReceivedBytes();
+    mTrafficHandler.sendEmptyMessage(0);
+  }
+
+  private int getTotalReceivedBytes() {
+    String line;
+    String[] segs;
+    int received = 0;
+    int i;
+    int tmp = 0;
+    boolean isNum;
+    try {
+      FileReader fr = new FileReader("/proc/net/dev");
+      BufferedReader in = new BufferedReader(fr, 500);
+      while ((line = in.readLine()) != null) {
+        line = line.trim();
+        if (line.startsWith("rmnet") || line.startsWith("eth") || line.startsWith("wlan")) {
+          segs = line.split(":")[1].split(" ");
+          for (i = 0; i < segs.length; i++) {
+            isNum = true;
+            try {
+              tmp = Integer.parseInt(segs[i]);
+            } catch (Exception e) {
+              isNum = false;
+            }
+            if (isNum == true) {
+              received = received + tmp;
+              break;
+            }
+          }
+        }
+      }
+    } catch (IOException e) {
+      return -1;
+    }
+    return received;
+  }
+
+  private boolean getConnectAvailable() {
+    try {
+      ConnectivityManager connectivityManager = (ConnectivityManager) mContext
+          .getSystemService(Context.CONNECTIVITY_SERVICE);
+      if (connectivityManager.getActiveNetworkInfo().isConnected())
+        return true;
+      else
+        return false;
+    } catch (Exception ex) {
+    }
+    return false;
+  }
+
+  
+  public void update() {
+    mTrafficHandler.removeCallbacks(mRunnable);
+    mTrafficHandler.postDelayed(mRunnable, 1000);
+  }
+    
+  Runnable mRunnable = new Runnable() {
+    @Override
+    public void run() {
+      mTrafficHandler.sendEmptyMessage(0);
+    }
+  };
+
+  private void updateSettings(){   
+    ContentResolver resolver = mContext.getContentResolver();
+    showTraffic = (Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_SHOW_TRAFFIC, 1) == 1);
+    if (showTraffic && getConnectAvailable()) {
+        updateTraffic();           
+        setVisibility(View.VISIBLE);
+    } else {
+        setVisibility(View.GONE);
+    }
+  }
+}
